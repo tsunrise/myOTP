c3e643f637f4fde7564a3a09ce0740791cf0dba3
Tom Shen <tomshen@berkeley.edu>
2019-07-05 19:48:57 +0800

    add ts

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+import './App.css';
+
+interface AppProp {
+
+}
+
+interface AppState {
+
+}
+
+class App extends React.Component<AppProp, AppState> {
+    render() {
+        return <div className="App">
+            Hello World
+        </div>;
+
+    };
+}
+
+export default App;

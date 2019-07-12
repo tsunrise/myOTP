83ad011cc06b17397449a80004b3088582e210a0
Tom Shen <tomshen@berkeley.edu>
2019-07-12 23:06:37 +0800

    add debug options

--- src/components/Viewer/UrlTokenFrame.tsx
+++ src/components/Viewer/UrlTokenFrame.tsx
@@ -6,6 +6,23 @@ interface RouteParams{
     token: string
 }
 
+export class CustomReader {
+    params: {[propName: string]: string} = {};
+
+    constructor(customParam: string){
+        for (let param of customParam.split('&')) {
+            let [prop, val] = param.split(':');
+            this.params[prop] = val;
+        }
+    }
+
+    get paramObject(): {[propName: string]: string} {
+        return this.params;
+    }
+
+
+}
+
 
 export class UrlTokenFrame extends React.Component<RouteComponentProps<RouteParams>>{
     render(): React.ReactNode {
